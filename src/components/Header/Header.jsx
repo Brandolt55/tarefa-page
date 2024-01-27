e65acef66e4d2025e6d97852830668ba19344037
Brandolt55 <gabrielbrandolt55@gmail.com>
2024-01-27 16:35:53 -0300

    criando logica de claro e escuro com useState no meu componente header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
+import lightIcon from "../../../public/light.svg";
+import nightIcon from "../../../public/night.svg";
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className='Headerr'>
+    <div className={`Headerr ${isDarkMode ? 'dark-mode' : ''}`}>
       <div className='Headerr__organizacao'>
         <h3>Organização</h3>
       </div>
       <div className='Headerr__tarefas'>
         <h2>Tarefas</h2>
         <div className='caixa-tarefa'></div>
+      </div>
+      <div className='dark-mode-toggle'>
+        <button onClick={toggleDarkMode}>
+          <img src={isDarkMode ? nightIcon : lightIcon} alt="Modo Claro/Escuro" />
+        </button>
       </div>
     </div>
   );
